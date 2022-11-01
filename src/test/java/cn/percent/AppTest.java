1022209b27ed68f8f366424b96bb7196109be52c
pengju.zhang <pengju.zhang@percent.cn>
2022-11-01 10:31:44 +0800

    Initial commit

---
+++ src/test/java/cn/percent/AppTest.java
@@ -0,0 +1,14 @@
+package cn.percent;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+}
